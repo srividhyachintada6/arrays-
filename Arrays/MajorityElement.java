81b584c792640bad835928526b6374b4da53ae7e
Srividhya6 <srividhyachintada6@gmail.com>
2026-08-10 18:00:00 +0530

    Learn arrays and solve 5 problems

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,21 @@
+class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums));
+    }
+}
