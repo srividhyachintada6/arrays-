97617c1283b096898463dbb75f81924683a8acc0
Srividhya6 <srividhyachintada6@gmail.com>
2026-08-11 19:00:00 +0530

    Practice strings with 3 problems

---
+++ Aug-11-Strings/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
